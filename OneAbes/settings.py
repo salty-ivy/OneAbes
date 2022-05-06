d165a6772721f9cd6721de8a8377a5540ca59c13
4the4ryushin <aman2001mi@gmail.com>
2022-05-06 20:05:58 +0530

    posts, users, clubs added

--- OneAbes/settings.py
+++ OneAbes/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'clubs.apps.ClubsConfig',
+    'posts.apps.PostsConfig',
+    'users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
@@ -116,7 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR/'static'
+STATICFILES_DIRS = [
+    BASE_DIR/'static',
+]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR/'media'
