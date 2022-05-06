2156f24a9021291ca0bc91772df4d8d4feb6c525
4the4ryushin <aman2001mi@gmail.com>
2022-05-06 19:43:50 +0530

    second commit

--- OneAbes/settings.py
+++ OneAbes/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-b*ubg+6sx!^qay9t#+6c+qf)22q^=$b-$qlgwjny8w71c0zq_&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'OneAbes.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR/'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,6 +116,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR/'static'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR/'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
